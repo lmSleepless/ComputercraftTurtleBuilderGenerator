476d050d89dc6cb488930c30bd72ecf12b877b07
Ryan Wilson <rpwilson1@gmail.com>
2013-03-26 07:58:48 -0500

    Initial commit

---
+++ ComputercraftTurtleBuilderGenerator/Program.cs
@@ -0,0 +1,12 @@
+using System;
+//An application that can look at a blueprint and generate an appropriate lua script for a turtle to build it
+namespace ComputercraftTurtleBuilderGenerator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
